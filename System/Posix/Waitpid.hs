e9879843b6b34b65e5e96d40f14eff9ce06b18e4
Niklas Hambüchen <mail@nh2.me>
2019-02-09 23:09:19 +0100

    Refactor: Rename status -> result.
    That is because it's not really an exit or termination status
    as per C terminology, but our custom result with a mask of
    `0x10000`, or `0x20000` etc. applied.

--- System/Posix/Waitpid.hs
+++ System/Posix/Waitpid.hs
@@ -17,17 +17,17 @@ data Flag = NoHang | IncludeUntraced | IncludeContinued deriving Show
 data Status = Exited Int | Signaled Signal | Stopped Signal | Continued deriving (Show, Eq)
 
 waitpid :: CPid -> [Flag] -> IO (Maybe (CPid, Status))
-waitpid pid flags = alloca $ \status -> do
-  child <- throwErrnoIfMinus1 "waitpid" $ c_waitpid pid status options
-  stat <- peek status
-  return $ guard (child /= 0) >> return (child, extractStatus stat)
+waitpid pid flags = alloca $ \resultPtr -> do
+  child <- throwErrnoIfMinus1 "waitpid" $ c_waitpid pid resultPtr options
+  result <- peek resultPtr
+  return $ guard (child /= 0) >> return (child, extractResult result)
  where
   options = foldl' (.|.) 0 (map flagValue flags)
   flagValue NoHang = 1
   flagValue IncludeUntraced = 2
   flagValue IncludeContinued = 4
-  extractStatus stat | stat < 0x10000 = Exited (fromIntegral stat)
-                     | stat < 0x20000 = Signaled (stat - 0x10000)
-                     | stat < 0x30000 = Stopped (stat - 0x20000)
-                     | stat == 0x30000 = Continued
-                     | otherwise = error $ "waitpid: unexpected status " ++ show stat
+  extractResult res | res < 0x10000 = Exited (fromIntegral res)
+                    | res < 0x20000 = Signaled (res - 0x10000)
+                    | res < 0x30000 = Stopped (res - 0x20000)
+                    | res == 0x30000 = Continued
+                    | otherwise = error $ "waitpid: unexpected result " ++ show res
